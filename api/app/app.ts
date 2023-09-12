e3348226073b57ce26028cac573b08f3d38f4134
hengage <tunecarterkillz@gmail.com>
2023-09-13 00:12:33 +0100

    Chore: use async await on db connection initialization

--- api/app/app.ts
+++ api/app/app.ts
@@ -1,61 +1,54 @@
 import express, {
-    Express,
-    Request,
-    Response,
-    NextFunction,
-    Router,
-  } from "express";
-  import { Server } from "http";
-  
-  import { dbConfig } from "../config/db.config";
-  
-  class App  {
-    public app: Express;
-    public router: Router;
-  
-    constructor() {
-      this.app = express();
-      this.router = Router();
-  
-      this.connectDB();
-      this.initializeMiddleware();
-      this.initializeRoutes();
-    }
-  
-    private connectDB = async () => {
-      try {
-        dbConfig.connect()
-          .then(() => {
-            console.log("conected to db");
-          })
-          .catch((error: any) => {
-            console.error(error);
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    };
-  
-    private initializeMiddleware() {
-      this.app.use(express.json());
-      this.app.use(express.urlencoded({ extended: true }));
-    }
-  
-  
-  
-    public initializeRoutes() {
-      this.app.get("/", (req: Request, res: Response) => {
-        res.send("Express typeScript app is set");
-      });
-  
-    //   this.app.use("/api/v1", routes.router);
-    }
-  
-    public listenToPort(port: string | number, node_env: string): Server {
-      return this.app.listen(`${port}`, () => {
-        console.log(`Server started at port ${port}. Current ENV is ${node_env}`);
-      });
-    }
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  Router,
+} from "express";
+import { Server } from "http";
+
+import { dbConfig } from "../config/db.config";
+
+class App {
+  public app: Express;
+  public router: Router;
+
+  constructor() {
+    this.app = express();
+    this.router = Router();
+
+    this.connectDB();
+    this.initializeMiddleware();
+    this.initializeRoutes();
   }
-  
-  export const app = new App();
+
+  private connectDB = async () => {
+    try {
+      await dbConfig.connect();
+       console.log("conected to db");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  private initializeMiddleware() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  public initializeRoutes() {
+    this.app.get("/", (req: Request, res: Response) => {
+      res.send("Express typeScript app is set");
+    });
+
+    //   this.app.use("/api/v1", routes.router);
+  }
+
+  public listenToPort(port: string | number, node_env: string): Server {
+    return this.app.listen(`${port}`, () => {
+      console.log(`Server started at port ${port}. Current ENV is ${node_env}`);
+    });
+  }
+}
+
+export const app = new App();
