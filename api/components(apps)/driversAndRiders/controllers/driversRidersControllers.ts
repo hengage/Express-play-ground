8569c61b8147cf5890c70f9ac49dae4c4c28c694
hengage <tunecarterkillz@gmail.com>
2023-09-25 11:27:21 +0100

    Feature: create service and controller for creating drivers and riders

---
+++ api/components(apps)/driversAndRiders/controllers/driversRidersControllers.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { driverRiderService } from "../services/driversRiders.services";
+import { jwtUtils } from "../../../utils";
+
+class DriverRiderController {
+  public async signup(req: Request, res: Response) {
+    const accountType = req.params.accountType;
+    try {
+      if (!accountType) {
+        return res.status(400).json({
+          message: "Account type is required",
+        });
+      }
+      const driverRider = await driverRiderService.signup(
+        req.body,
+        accountType
+      );
+      const payload = {
+        phoneNumber: driverRider.phoneNumber,
+        _id: driverRider._id,
+      };
+      const accessToken = jwtUtils.generateToken(payload, "1h");
+      return res.status(201).json({
+        message: "Account created successfully",
+        data: {
+        ...driverRider,
+          accessToken,
+        },
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        message: "Failed to create acount",
+        error: error.message,
+      });
+    }
+  }
+}
--- api/components(apps)/driversAndRiders/services/driversRiders.services.ts
+++ api/components(apps)/driversAndRiders/services/driversRiders.services.ts
@@ -2,49 +2,86 @@ import { HandleException } from "../../../utils";
 import { DriverRider } from "../models/driversRiders.models";
 import { IDriverRider } from "../models/driversRiders.models.interface";
 
-class DriverRidersService {
-        async getDriverOrRiderByPhoneNumber(
-            phoneNumber: string,
-            selectFields?: string
-          ): Promise<IDriverRider> {
-            try {
-              const query = DriverRider.findOne({
-                phoneNumber: { $eq: phoneNumber },
-              });
-        
-              if (selectFields) {
-                query.select(selectFields);
-              }
-        
-              const driverRider = await query.exec();
-              if (!driverRider) {
-                throw new HandleException(401, "Driver or rider not found");
-              }
-              return driverRider;
-            } catch (error: any) {
-              throw new Error(error.message);
-            }
+class DriverRiderService {
+  async getDriverOrRiderByPhoneNumber(
+    phoneNumber: string,
+    selectFields?: string
+  ): Promise<IDriverRider> {
+    try {
+      const query = DriverRider.findOne({
+        phoneNumber: { $eq: phoneNumber },
+      });
+
+      if (selectFields) {
+        query.select(selectFields);
+      }
+
+      const driverRider = await query.exec();
+      if (!driverRider) {
+        throw new HandleException(401, "Driver or rider not found");
+      }
+      return driverRider;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
-        async getDriverOrRiderById(
-            _id: string,
-            selectFields?: string
-          ): Promise<IDriverRider> {
-            try {
-              const query = DriverRider.findById(_id)
-        
-              if (selectFields) {
-                query.select(selectFields);
-              }
-        
-              const driverRider = await query.exec();
-              if (!driverRider) {
-                throw new HandleException(401, "Driver or rider not found");
-              }
-              return driverRider;
-            } catch (error: any) {
-              throw new Error(error.message);
-            }
+  }
+  async getDriverOrRiderById(
+    _id: string,
+    selectFields?: string
+  ): Promise<IDriverRider> {
+    try {
+      const query = DriverRider.findById(_id);
+
+      if (selectFields) {
+        query.select(selectFields);
+      }
+
+      const driverRider = await query.exec();
+      if (!driverRider) {
+        throw new HandleException(401, "Driver or rider not found");
+      }
+      return driverRider;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
+  }
+
+  async signup(payload: any, accountType: string) {
+    let middleName;
+    if (payload.middleName) {
+      middleName = payload.middleName;
+    } else {
+      middleName = null;
+    }
+    try {
+      const newDriverRider = new DriverRider({
+        name: {
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          middleName,
+        },
+        email: payload.email,
+        phoneNumber: payload.phoneNumber,
+        password: payload.password,
+        accountType,
+        vehicleType: payload.vehicleType,
+        vehicleInsurancePhoto: payload.vehicleInsurancePhoto,
+        licenseNumber: payload.licenseNumber,
+        govtIdPhoto: payload.govtIdPhoto,
+        address: {
+            street: payload.street,
+            city: payload.city,
+            state: payload.state,
+            country: payload.country,
+            zipCode: payload.zipCode,
+        }
+      });
+      const savedDriverrider = newDriverRider.save();
+      return savedDriverrider;
+    } catch (error: any) {
+        throw new HandleException(500, error.message);
+    }
+  }
 }
 
-export const driverRidersService = new DriverRidersService();
+export const driverRiderService = new DriverRiderService();
