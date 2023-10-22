8aea0a36495db7924476491af41c0c7f3a39cffd
hengage <tunecarterkillz@gmail.com>
2023-10-22 22:29:31 +0100

    Feature: shop middleware

--- api/components(apps)/shops/index.ts
+++ api/components(apps)/shops/index.ts
@@ -1,2 +1,3 @@
+export { Shop } from "./models/shops.models";
 export { shopServices } from "./services/shops.services";
 export { shopsRoutes } from "./routes/shops.routes";
---
+++ api/middleware/shop.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtUtils } from "../utils";
+import { Shop } from "../components(apps)/shops";
+import { STATUS_CODES } from "../constants";
+import { JwtPayload } from "jsonwebtoken";
+
+class ShopMiddleware {
+  public async checkShopOwnerShip(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const decodedUser = jwtUtils.verifyToken(req) as JwtPayload
+    //   const vendor = req.params.vendorId;
+      const shop = await Shop.findById(req.params.shopId).select('_id vendor');
+
+      // The vendor is the owner and can perform the action
+      if (shop?.vendor.toString() === decodedUser._id) {
+        next();
+        return;
+      }
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to access this resource" });
+    } catch (error: any) {
+      res.status(error.status || STATUS_CODES.SERVER_ERROR).json({
+        message: "Operartion failed",
+        error: error.message,
+      });
+    }
+  }
+}
+
+export const shopMiddleware = new ShopMiddleware();
