9553055606727686c29ecfc7f907b247b827418b
hengage <tunecarterkillz@gmail.com>
2023-10-28 18:46:52 +0100

    Feature: change password

--- api/components(apps)/auth/controllers/passwordMgmt.controller.ts
+++ api/components(apps)/auth/controllers/passwordMgmt.controller.ts
@@ -4,21 +4,37 @@ import { STATUS_CODES } from "../../../constants";
 
 class PasswordMgmtController {
   public async resetPassword(req: Request, res: Response) {
-    const  userType   = req.query.usertype as string;
+    const userType = req.query.usertype as string;
     try {
       await passwordMgmtService.resetPassword(
         req.body.phoneNumber,
         req.body.newPassword,
         userType.toLowerCase()
       );
-      res.status(STATUS_CODES.OK)
-      .json({message: "Password reset successful"})
-    } catch (error: any ) {
-        res.status(error.status|| STATUS_CODES.SERVER_ERROR)
-        .json({
-            message: "Password reset failed",
-            error: error.message
-        })
+      res
+        .status(STATUS_CODES.OK)
+        .json({ message: "Password reset successful" });
+    } catch (error: any) {
+      res.status(error.status || STATUS_CODES.SERVER_ERROR).json({
+        message: "Password reset failed",
+        error: error.message,
+      });
+    }
+  }
+
+  public async changePassword(req: Request, res: Response) {
+    const userType = req.query.usertype as string;
+    const { userId, newPassword } = req.body;
+    try {
+      await passwordMgmtService.changePassword(userId, newPassword, userType);
+      res.status(STATUS_CODES.OK).json({
+        message: "Password changed"
+      })
+    } catch (error: any) {
+      res.status(error.status || STATUS_CODES.SERVER_ERROR).json({
+        message: "Password chnage failed",
+        error: error.message,
+      });
     }
   }
 }
--- api/components(apps)/auth/services/passwordMgmt.services.ts
+++ api/components(apps)/auth/services/passwordMgmt.services.ts
@@ -1,5 +1,5 @@
 import { STATUS_CODES } from "../../../constants";
-import { HandleException } from "../../../utils";
+import { HandleException, encryption } from "../../../utils";
 import { Customer } from "../../customers";
 import { DriverRider } from "../../driversAndRiders";
 import { Vendor } from "../../vendors";
@@ -18,6 +18,30 @@ class PasswordMgmtService {
 
     user.password = newPassword;
     user.save();
+  }
+
+  public async changePassword(userId: string, newPassword: string, userType: string) {
+    try {
+      const UserModel = await this.getUserModel(userType);
+      const user = await (UserModel as any).findById(userId);
+
+      const currentPasswordMatch = await encryption.compareValues(
+        newPassword,
+        user.password
+      );
+
+      if (!currentPasswordMatch) {
+        throw new HandleException(
+          STATUS_CODES.BAD_REQUEST,
+          "Incorrect password"
+        );
+      }
+      user.password = newPassword
+      user.save();
+      return;
+    } catch (error: any) {
+        throw new HandleException(error.status, error.message);
+    }
   }
 
   private async getUserModel(userType: string) {
