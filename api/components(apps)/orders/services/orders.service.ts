29336e978a83a50c65fe26ec59ca3e671a4fbc93
hengage <tunecarterkillz@gmail.com>
2023-11-04 08:41:37 +0100

    Chore: return only relevant data on create order

--- api/components(apps)/orders/services/orders.service.ts
+++ api/components(apps)/orders/services/orders.service.ts
@@ -2,29 +2,38 @@ import { HandleException } from "../../../utils";
 import { Order } from "../models/orders.models";
 
 class OrdersService {
-    public async createOrder(payload: any) {
-        try {
-            const orderItems = payload.items.map((item: any) => {
-                return {
-                  product: item.product,
-                  quantity: item.quantity,
-                  price: item.price,
-                  shop: item.shop, 
-                };
-              });
-        
-            const order = new Order({
-                customer: payload.customer,
-                items: orderItems,
-                totalAmount: payload.totalAmount
-            })
+  public async createOrder(payload: any) {
+    try {
+      const orderItems = payload.items.map((item: any) => {
+        return {
+          product: item.product,
+          quantity: item.quantity,
+          price: item.price,
+          shop: item.shop,
+        };
+      });
 
-            await order.save()
-            return order
-        } catch (error: any) {
-            throw new HandleException(error.status, error.message)
-        }
+      const newOrder = new Order({
+        customer: payload.customer,
+        items: orderItems,
+        totalAmount: payload.totalAmount,
+      });
+
+      await newOrder.save();
+
+      const order = {
+        _id: newOrder._id,
+        cuatomer: newOrder.customer,
+        items: newOrder.items,
+        totalAmount: newOrder.totalAmount,
+        status: newOrder.status,
+        createdAt: newOrder.createdAt,
+      };
+      return order;
+    } catch (error: any) {
+      throw new HandleException(error.status, error.message);
     }
+  }
 }
 
-export const ordersService = new OrdersService()
+export const ordersService = new OrdersService();
