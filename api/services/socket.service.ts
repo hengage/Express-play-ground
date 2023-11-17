dcc8c6d3a2a4ee53a15c6a6785048f511856dfaf
hengage <tunecarterkillz@gmail.com>
2023-11-17 22:47:33 +0100

    Feature: create vendor room

--- api/services/socket.service.ts
+++ api/services/socket.service.ts
@@ -7,6 +7,7 @@ class InitializeWebSocket {
     public async connectSocket(server: Server ) {
         const io = socketIO(server);
 
+        let vendor: string;
         io.on('connection', (socket: Socket) =>{
             console.log('User connected')
 
@@ -14,8 +15,17 @@ class InitializeWebSocket {
                 console.log('Message received', message)
             })
 
+            socket.on('create-vendor-room', async (message) =>{
+                vendor = message.vendorId
+                console.log('Message received', message.vendorId)
+                socket.join(message.vendorId)
+            })
+
+          
+
             socket.on('disconnect', () =>{
                 console.log('User disconnected')
+                socket.leave(vendor)
             })
         })
     }
