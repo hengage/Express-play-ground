c177de501e5709cfa100807133107e0fa2034fd9
hengage <tunecarterkillz@gmail.com>
2023-11-20 18:11:12 +0100

    Feature: notification

--- api/server.ts
+++ api/server.ts
@@ -3,16 +3,9 @@ dotenv.config();
 
 import { app } from "./app/app";
 import { PORT, NODE_ENV } from "./config/secrets.config";
-import { initializeWebSocket, notificationService } from "./services";
+import { initializeWebSocket } from "./services";
 
-const message = {
-    notification: {
-      title: 'New message',
-      body: 'You have a new message from John',
-    },
-    token: 'device_token',
-  };
-notificationService.sendNotification(message)
+
 
 const server = app.listenToPort(PORT, NODE_ENV);
 initializeWebSocket.connectSocket(server)
--- api/services/socket.service.ts
+++ api/services/socket.service.ts
@@ -1,34 +1,45 @@
 import { Server } from "http";
 
-const socketIO = require('socket.io');
+const socketIO = require("socket.io");
 import { Socket } from "socket.io";
+import { notificationService } from "./notification.service";
 
 class InitializeWebSocket {
-    public async connectSocket(server: Server ) {
-        const io = socketIO(server);
+  public async connectSocket(server: Server) {
+    const io = socketIO(server);
 
-        let vendor: string;
-        io.on('connection', (socket: Socket) =>{
-            console.log('User connected')
+    let vendor: string;
+    io.on("connection", (socket: Socket) => {
+      console.log("User connected");
 
-            socket.on('message', (message) =>{
-                console.log('Message received', message)
-            })
+      socket.on("message", (message) => {
+        console.log("Message received", message);
+      });
 
-            socket.on('create-vendor-room', async (message) =>{
-                vendor = message.vendorId
-                console.log('Message received', message.vendorId)
-                socket.join(message.vendorId)
-            })
+      socket.on("create-vendor-room", async (message) => {
+        vendor = message.vendorId;
+        console.log("Message received", message.vendorId);
+        socket.join(message.vendorId);
+      });
 
-          
+      socket.on("send-order", async (message) => {
+        const payload = {
+          notification: {
+            title: "New message",
+            body: "You have a new message from John",
+          },
+          token: "device_token",
+        };
 
-            socket.on('disconnect', () =>{
-                console.log('User disconnected')
-                socket.leave(vendor)
-            })
-        })
-    }
+        notificationService.sendNotification(payload);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+        socket.leave(vendor);
+      });
+    });
+  }
 }
 
-export const initializeWebSocket = new InitializeWebSocket()
+export const initializeWebSocket = new InitializeWebSocket();
