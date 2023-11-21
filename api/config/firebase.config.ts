9683eba982afd909528754c04938997d0be97c4d
hengage <tunecarterkillz@gmail.com>
2023-11-21 02:30:59 +0100

    Chore: set and use firebase service account as an env file

--- api/config/firebase.config.ts
+++ api/config/firebase.config.ts
@@ -1,7 +1,21 @@
 // const admin = require('firebase-admin');
+const fs = require('fs');
+
 import * as admin from "firebase-admin";
 
-const serviceAccount = require("./mr-delivery-ghana-d2aa0-firebase-adminsdk-qune8-77567bd46c.json");
+
+const filePath = process.env.FIREBASE_ADMIN_SDK_KEY_MrD;
+
+if (!filePath) {
+  console.error("FIREBASE_ADMIN_SDK_KEY_MrD environment variable is not set.");
+  process.exit(1);
+}
+
+// Read the content of the file
+const serviceAccountJson = fs.readFileSync(filePath, 'utf8');
+
+console.log("Raw content of FIREBASE_ADMIN_SDK_KEY_MrD:", serviceAccountJson);
+const serviceAccount = JSON.parse(serviceAccountJson)
 
 export const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
