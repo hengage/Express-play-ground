7ca9d34b43970c453ed65c8f17520062600df039
hengage <tunecarterkillz@gmail.com>
2023-12-05 04:22:04 +0100

    Feature: trip model

--- api/components(apps)/maku/maku.interfaces.ts
+++ api/components(apps)/maku/maku.interfaces.ts
@@ -7,3 +7,22 @@ export interface IVehicleType extends Document {
     feePerKM: string
     riderPercentage: string
 }
+
+export interface ITrip extends Document {
+    _id: string;
+    customer: string;
+    driver: string;
+    pickupLocation: {
+        locationType: string;
+        coordinates: [number, number];
+    };
+    destination: {
+        locationType: string;
+        coordinates: [number, number];
+    };
+    vehicleType: string;
+    price: string;
+    status: string;
+    date: string;
+
+}
--- api/components(apps)/maku/models/maku.model.ts
+++ api/components(apps)/maku/models/maku.model.ts
@@ -1,17 +1,58 @@
 import { model, Schema } from "mongoose";
-import { IVehicleType } from "../maku.interfaces";
+import { ITrip, IVehicleType } from "../maku.interfaces";
 import { uniqueString } from "../../../utils";
 
 const vehicleTypeSchema = new Schema<IVehicleType>({
-    _id: {
-        type: String,
-        required: true,
-        default: () => uniqueString.generateUniqueString(4),
-    },
-    vehicleType: {type: String, required: true},
-    baseFee: {type: String, required: true, },
-    feePerKM: {type: String, required: true},
-    riderPercentage: {type: String, required: true }
-})
+  _id: {
+    type: String,
+    required: true,
+    default: () => uniqueString.generateUniqueString(4),
+  },
+  vehicleType: { type: String, required: true },
+  baseFee: { type: String, required: true },
+  feePerKM: { type: String, required: true },
+  riderPercentage: { type: String, required: true },
+});
 
-export const VehicleType = model<IVehicleType>('VehicleType', vehicleTypeSchema)
+const tripSchema = new Schema<ITrip>(
+  {
+    _id: {
+      type: String,
+      required: true,
+      default: () => uniqueString.generateUniqueString(4),
+    },
+    customer: { type: String, required: true, ref: "Customer" },
+    driver: { type: String, required: true, ref: "DriverRider" },
+    pickupLocation: {
+      locationType: {
+        type: String,
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    destination: {
+      locationType: {
+        type: String,
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    vehicleType: { type: String, required: true },
+    price: { type: String, required: true },
+    status: { type: String, default: "" },
+  },
+  { timestamps: true, _id: false }
+);
+
+export const Trip = model<ITrip>("Trip", tripSchema);
+
+export const VehicleType = model<IVehicleType>(
+  "VehicleType",
+  vehicleTypeSchema
+);
