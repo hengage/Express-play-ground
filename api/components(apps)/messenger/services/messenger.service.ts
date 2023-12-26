302b6202e4c4b566608505c78a27642ee872bb86
hengage <tunecarterkillz@gmail.com>
2023-12-26 14:41:15 +0100

    Feature: Messenger Service - find and notify nearest riders

---
+++ api/components(apps)/messenger/services/messenger.service.ts
@@ -0,0 +1,39 @@
+import { findClosestDriverOrRider } from "../../../services";
+import { notificationService } from "../../notifications";
+import { IMessengerOrder } from "../messenger.interface";
+import { messengerRepo } from "../repository/messenger.repo";
+
+class Messengerservice {
+  private async notifyNearestRiders(
+    pickUpCoordinates: [number, number],
+    order: IMessengerOrder,
+    searchKMLimit: number
+  ) {
+    const riders = await findClosestDriverOrRider(
+      pickUpCoordinates,
+      "rider",
+      searchKMLimit
+    );
+
+    if (riders.length > 0) {
+      riders.forEach((rider) => {
+        notificationService.notifyRiderOfOrder(rider._id, order);
+      });
+    }
+  }
+
+  async createOrder(payload: any, searchKMLimit: number) {
+    const order = await messengerRepo.createOrder(payload);
+    if (order.scheduledPickUpTime) {
+      // scheduleMessengerPicku()
+    } else {
+      this.notifyNearestRiders(
+        payload.pickUpCoordinates,
+        payload,
+        searchKMLimit
+      );
+    }
+  }
+}
+
+export const messengerService = new Messengerservice();
