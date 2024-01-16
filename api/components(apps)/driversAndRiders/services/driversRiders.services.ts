5a6cc9bcdccc2d87f4a8d6b3d7b2a7f48800211f
hengage <tunecarterkillz@gmail.com>
2024-01-16 17:10:48 +0100

    CHore: update driver/rider error message

--- api/components(apps)/driversAndRiders/services/driversRiders.services.ts
+++ api/components(apps)/driversAndRiders/services/driversRiders.services.ts
@@ -165,7 +165,7 @@ class DriverRiderService {
     const driver = await DriverRider.findById(id).select("location");
 
     if (!driver) {
-      throw new HandleException(STATUS_CODES.NOT_FOUND, "Cannot find driver");
+      throw new HandleException(STATUS_CODES.NOT_FOUND, "Cannot find driver/rider");
     }
     driver.location.coordinates = coordinates;
     await driver.save();
